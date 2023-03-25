f1f8a6b8f2148eb3524bb2b2c56aed01a0e66ea3
Thanos-83 <s_sbonias@hotmail.com>
2023-03-25 16:07:33 +0200

    fetch product categories on the client

--- pages/index.js
+++ pages/index.js
@@ -6,18 +6,16 @@ import axios from 'axios';
 import Link from 'next/link';
 import Image from 'next/image';
 import { AddNode, DisplayLeafs, SearchNode } from '../utils/test';
-export default function Home({ categories }) {
-  // const [productCategories, setProductCategories] = useState([]);
-  // useEffect(() => {
-  //   axios
-  //     .get('/api/dashboard/categories')
-  //     .then((res) => setProductCategories(res.data));
-  // }, []);
+export default function Home() {
+  const [productCategories, setProductCategories] = useState([]);
+
   useEffect(() => {
-    // AddNode();
-    // DisplayLeafs('Computers & Electronics');
-    // const x = SearchNode('Computers & Electronics');
-    // console.log(x);
+    axios
+      .get(`${process.env.NEXT_PUBLIC_URL}/api/dashboard/categories`)
+      .then((response) => setProductCategories(response.data))
+      .catch((error) =>
+        console.log('Error fetching product categories: ', error)
+      );
   }, []);
   return (
     <>
@@ -33,7 +31,7 @@ export default function Home({ categories }) {
           </button>
           <h1 className='text-3xl my-8'>This is the home page</h1>
           <ul className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4'>
-            {categories?.map((category) => (
+            {productCategories?.map((category) => (
               <li key={category._id} className='relative'>
                 <Link
                   href={`/shop/${category.slug}`}
@@ -61,17 +59,17 @@ export default function Home({ categories }) {
   );
 }
 
-export async function getServerSideProps() {
-  // AddNode();
-  // const x = SearchNode('Computers & Electronics');
-  // console.log(x);
-  const categories = await axios.get(
-    `${process.env.NEXT_PUBLIC_URL}/api/dashboard/categories`
-  );
-  // console.log('Categories: ', categories.data);
-  return {
-    props: {
-      categories: categories.data,
-    },
-  };
-}
+// export async function getServerSideProps() {
+//   // AddNode();
+//   // const x = SearchNode('Computers & Electronics');
+//   // console.log(x);
+//   const categories = await axios.get(
+//     `${process.env.NEXT_PUBLIC_URL}/api/dashboard/categories`
+//   );
+//   // console.log('Categories: ', categories.data);
+//   return {
+//     props: {
+//       categories: categories.data,
+//     },
+//   };
+// }
